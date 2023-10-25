d18b08bf3279162cb0ec60782dcc3725797f8fe3
Ye <ocean.yef@gmail.com>
2023-10-25 11:03:44 +0800

    测试同步

--- ydb转换.py
+++ ydb转换.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 # coding:utf-8
 
+import tinker
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
